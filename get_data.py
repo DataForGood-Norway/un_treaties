b83be60cc3d4f4c4448bb7320fa272e8b57bc756
Geir Arne Hjelle <geirarne@gmail.com>
2018-04-20 19:27:09 +0200

    Added some parsing from top of page

--- get_data.py
+++ get_data.py
@@ -1,13 +1,17 @@
+import itertools
+
 import bs4
+import pandas as pd
 import requests
-import pandas
 
 BASE = 'https://treaties.un.org/Pages/'
 START_PAGE = 'ParticipationStatus.aspx?clang=_en'
 
+
 def get_soup(url):
     text = requests.get(url).text
     return bs4.BeautifulSoup(text, 'lxml')
+
 
 def links_containing(text):
     def filter(tag):
@@ -15,38 +19,53 @@ def links_containing(text):
                 text in tag.get('href', ''))
     return filter
 
-def make_df(url, name):
+
+def make_df(url):
     """Make a data frame from a treaty URL."""
-    print(url, name)
-    treaty = get_soup(url)
+    print(url)
+    treaty_soup = get_soup(url)
+
+    # Parse the table of countries
     def resp_table_div(tag):
         return (tag.name == 'div' and
                 'table-responsive' in tag.get('class', ''))
-    df = pandas.read_html(str(treaty.find(resp_table_div)),
-                          header=[0],
-                          parse_dates=[1])[0]
-    df['Treaty'] = name
+    df = pd.read_html(str(treaty_soup.find(resp_table_div)), header=[0],
+                      parse_dates=[1])[0]
+
+    # Get stuff from the top of the page
+    fields = dict(
+        Treaty='treatyCenter',
+        Chapter='tcTextCenter',
+        Date='treatyCenterSub',
+        )
+    for field, class_ in fields.items():
+        field_text = treaty_soup.find(class_=class_).text.strip()
+        df[field] = field_text.split('\n')[-1].strip()
+
+    # Get notes from bottom table
+    #   TODO
+
+    # Other useful stuff
     df['URL'] = url
+
     return df
 
+
 def process(df):
-    pass
+    return df
+
+
 
 def iter_treaties():
-    base = get_soup(BASE + START_PAGE)
-    for chapter_link in base(links_containing('Treaties.aspx')):
+    base_soup = get_soup(BASE + START_PAGE)
+    for chapter_link in base_soup(links_containing('Treaties.aspx')):
         chapter_soup = get_soup(BASE + chapter_link['href'])
         treaty_links = chapter_soup(links_containing('Details.aspx'))
         for link in treaty_links:
-            name = link.text
             treaty_url = BASE + link['href']
-            try:
-                yield make_df(treaty_url, name)
-            except:
-                continue
+            yield process(make_df(treaty_url))
+
 
 if __name__ == '__main__':
-    import itertools
-    for df in itertools.islice(iter_treaties(), 5):
+    for df in iter_treaties():  # itertools.islice(iter_treaties(), 5):
         print(df.head())
-            
